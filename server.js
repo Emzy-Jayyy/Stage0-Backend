61eacd3d34981c7d55f460b2a6786547e979ae83
Emzy-Jayyy <emediongb23@gmail.com>
2025-10-18 17:12:16 +0100

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
     res.status(404).json({ message: 'Route not found' });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
